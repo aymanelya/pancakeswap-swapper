48790da874436addad09ced058d73c7354bec226
Ayman Elyahmidi <aymanelyahmidi@gmail.com>
2023-08-16 19:16:46 +0200

    functional quickswap V2 AND V3

--- abis/index.js
+++ abis/index.js
@@ -1,7 +1,15 @@
 const quickswapV2RouterAbi = require('./quickswapV2RouterAbi.json');
+const quickswapV3RouterAbi = require('./quickswapV3RouterAbi.json');
+const quickswapV3FactoryAbi = require('./quickswapV3FactoryAbi.json');
+const quickswapV3PoolAbi = require('./quickswapV3PoolAbi.json');
 const ERC20 = require('./ERC20.json');
+const WMATIC = require('./WMATIC.json');
 
 module.exports = {
   quickswapV2RouterAbi,
+  quickswapV3RouterAbi,
+  quickswapV3FactoryAbi,
+  quickswapV3PoolAbi,
   ERC20,
+  WMATIC,
 };
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { ethers } = require('ethers');
 let provider = new ethers.providers.JsonRpcProvider(config[config.network].RPC);
 // connect the proper wallet
 const wallet = new ethers.Wallet(config.userWallet.privateKey, provider);
+const abi = new ethers.utils.AbiCoder();
 
 async function main() {
   // Fetch current gas price from the network
@@ -119,8 +120,133 @@ async function main() {
 
     console.log(swapResult);
   } else if (config.quickSwapVersion == 3) {
+    console.log('USING QUICKSWAP V3 !');
+    const quickswapV3RouterContract = new ethers.Contract(
+      config[config.network].addresses.quickswapV3Router,
+      abis.quickswapV3RouterAbi,
+      wallet
+    );
+
+    const deadline = Math.floor(Date.now() / 1000 + 60 * 10);
+
+    const amountIn = ethers.utils.parseUnits(
+      config.swapParams.amountIn,
+      config.swapParams.tokenInDecimals
+    );
+    const amountOutMin = ethers.utils.parseUnits(
+      config.swapParams.amountOutMin,
+      config.swapParams.tokenOutDecimals
+    );
+
+    let swapResult;
+
+    if (config.swapParams.swapMethod == 'tokenToToken') {
+      console.log('APPROVING TOKENIN...');
+
+      const tokenInContract = new ethers.Contract(
+        config.swapParams.tokenInAddress,
+        abis.ERC20,
+        wallet
+      );
+      // Approve token
+      const approveTx = await tokenInContract.approve(
+        config[config.network].addresses.quickswapV3Router,
+        amountIn,
+        {
+          gasPrice,
+        }
+      );
+      await approveTx.wait();
+      console.log('SWAPPING TOKEN TO TOKEN...');
+
+      swapResult = await quickswapV3RouterContract.exactInputSingle(
+        {
+          tokenIn: config.swapParams.tokenInAddress,
+          tokenOut: config.swapParams.tokenOutAddress,
+          recipient: config.userWallet.address,
+          deadline,
+          amountIn,
+          amountOutMinimum: amountOutMin,
+          limitSqrtPrice: 0,
+        },
+        { gasPrice }
+      );
+
+      console.log('sent TX');
+    } else if (config.swapParams.swapMethod == 'maticToToken') {
+      console.log('SWAPPING MATIC TO TOKEN...');
+
+      swapResult = await quickswapV3RouterContract.exactInputSingle(
+        {
+          tokenIn: config.swapParams.tokenInAddress,
+          tokenOut: config.swapParams.tokenOutAddress,
+          recipient: config.userWallet.address,
+          deadline,
+          amountIn,
+          amountOutMinimum: amountOutMin,
+          limitSqrtPrice: 0,
+        },
+        { gasPrice, value: amountIn }
+      );
+
+      console.log('sent TX');
+    } else if (config.swapParams.swapMethod == 'tokenToMatic') {
+      console.log('APPROVING TOKENIN...');
+
+      const tokenInContract = new ethers.Contract(
+        config.swapParams.tokenInAddress,
+        abis.ERC20,
+        wallet
+      );
+      // Approve token
+      const approveTx = await tokenInContract.approve(
+        config[config.network].addresses.quickswapV3Router,
+        amountIn,
+        {
+          gasPrice,
+        }
+      );
+      await approveTx.wait();
+      console.log('SWAPPING TOKEN TO WMATIC...');
+
+      swapResult = await quickswapV3RouterContract.exactInputSingle(
+        {
+          tokenIn: config.swapParams.tokenInAddress,
+          tokenOut: config.swapParams.tokenOutAddress,
+          recipient: config.userWallet.address,
+          deadline,
+          amountIn,
+          amountOutMinimum: amountOutMin,
+          limitSqrtPrice: 0,
+        },
+        { gasPrice }
+      );
+    }
+
+    await swapResult.wait();
+
+    if (config.swapParams.swapMethod == 'tokenToMatic') {
+      console.log('WITHDRAWING WMATIC TO MATIC...');
+      const wmaticContract = new ethers.Contract(
+        config.swapParams.tokenOutAddress,
+        abis.WMATIC,
+        wallet
+      );
+      const wmaticBalance = await wmaticContract.balanceOf(
+        config.userWallet.address
+      );
+
+      const tx = await wmaticContract.withdraw(wmaticBalance, {
+        gasPrice,
+      });
+      console.log('sent TX');
+      await tx.wait();
+      console.log(tx);
+    } else {
+      console.log(swapResult);
+    }
   } else {
-    console.log('Wrong quiswapVersion number');
+    console.log('Wrong quickswapVersion number');
   }
 }
 
